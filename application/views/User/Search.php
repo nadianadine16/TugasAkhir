41028686bb66cdd4c8423b2d260c23ed1d7d6d05
shevaputriw <shevapw@gmail.com>
2021-05-23 13:50:43 +0700

    Tampilan Hasil Cari Forum

--- application/views/User/Search.php
+++ application/views/User/Search.php
@@ -1,37 +1,51 @@
-<section id="team" class="team section-bg" style="margin-top:50px">
-      <div class="container">
-        <div class="section-title">
-          <h2>Daftar Forum</h2>
-          <p>Jika terdapat pertanyaan yang belum dibahas pada forum dapat menekan button tanya forum</p>
-        </div>
-        <div class="col-lg-12 pt-12 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-        <div><a href="<?= base_url();?>User/Tanya_Forum" class="btn btn-primary" style="margin-bottom:10px; float:right">Tanya Forum</a></div>        
-      </div>
-      <div class="row">      
+<section id="team" class="team section" style="margin-top:50px; ">
+  <div class="container" style="background-color:#f4fbfe; margin-top:-30px;">
+    <div class="section-title">
+      <h2><br>Forum Diskusi</h2>
+      <p>Jika terdapat pertanyaan yang belum dibahas pada forum dapat menekan button tanya forum</p>
+    </div>
+      
+    <div class="row">      
       <div class="col-md-5">
         <form action ="<?= base_url('user/cari');?>" method="post">
           <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search.." name="keyword" autocomplete="off">
+            <input type="text" class="form-control" placeholder="Search.." name="keyword" autocomplete="off" autofocus>
             <div class="input-group-append">
               <input class="btn btn-primary" type="submit" name="submit"></button>
             </div>
           </div>
         </form>
       </div>
-    </div>
-        <div class="row" style="margin-rop:15px;">        
+      <span><div><a href="<?= base_url();?>User/Tanya_Forum" class="btn btn-primary" style="margin-left:365%;">+ Tanya Forum</a></div></span>
+    </div><br>
+
+    <?php if($forum == NULL) {?>
+      <center><p>Kata kunci yang Anda cari tidak ditemukan.<b> Silahkan coba lagi.</b><br><br><br></p></center>
+    <?php }
+    else {?>
+      <div class="row" style="margin-rop:15px;">
         <?php $no=1; foreach($forum as $p):?>
+          <?php $tanggal = $p["created_at"]?>
           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title" style="font-size:15px;"><b>Nama Penanya : </b> <?=$p["nama"];?></h5>
-                <p class="card-text" style="font-size:13px;"><b>Pertanyaan : </b><?=$p["pertanyaan"];?></p>
-                <p class="card-text" style="font-size:13px;text-align: right;"><?=$p["created_at"];?></p>
+                <h5 class="card-title" style="font-size:15px;"><b>Kategori : </b> <?=$p["nama_kategori"];?></h5>
+                <p class="card-text" style="font-size:13px;"><b>Pertanyaan : </b><?= substr($p["pertanyaan"],0,100)?></p>
+                <p class="card-text" style="font-size:13px;text-align: right;"><?php echo date("d-F-Y", strtotime($tanggal));?></p>
                 <a href="<?= base_url();?>user/Detail_Forum/<?=$p['id_forum'];?>" class="btn btn-primary" style="float:right">Lihat Forum</a>
               </div>
-            </div>
+            </div><br>
           </div>
         <?php endforeach;?>
-        </div>
       </div>
-    </section><!-- End Team Section -->
+
+      <!-- <div class="row">
+        <div class="col">
+          Tampilkan pagination
+          <?php echo $pagination; ?>
+        </div>
+      </div> -->
+    <?php } ?>
+  </div>
+</section>
